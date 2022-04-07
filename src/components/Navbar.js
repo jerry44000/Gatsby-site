0c54e031c6afc0f87bad6a6515af232df69a2556
jerry44000 <shai.badarna@gmail.com>
2022-04-07 13:39:52 +0200

    correction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,6 @@ const Navbar = () => {
         <div className="nav-link fs-5 text-dark">
           <Link to="#home" activeClassName="active">Home</Link>
           <Link to="#projects" activeClassName="active">Projects</Link>
-          <Link to="#pricing" activeClassName="active">Pricing</Link>
           <Link to="#contact" activeClassName="active">Contact</Link>
         </div>
       </nav>
